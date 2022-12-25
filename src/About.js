b344513204e2690173e2b336812b67556cdc7390
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2022-12-25 13:10:43 -0800

    about starter

---
+++ src/About.js
@@ -0,0 +1,22 @@
+import React from'react';
+import "./About.css";
+
+function About() {
+    return (
+      <div className="about">
+        <h1>About Us</h1>
+        <p>Hello, </p>
+        <br />
+        <p>
+          Blah Blah
+        </p>
+        <br />
+        <p>
+        Blah Blah
+        </p>
+ 
+      </div>
+    );
+  }
+  
+  export default About;
