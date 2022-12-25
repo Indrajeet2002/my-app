77e5df7dc4808e4fbcf1873ca122557e9b2b6805
falshami2002 <alshamifiras@yahoo.com>
2022-12-24 22:31:35 -0800

    Switched to small navbar and changed footer layout

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,15 +5,10 @@ import {HiInformationCircle} from 'react-icons/hi2';
 
 const Navbar = () => {
     return (
-        <div className='top-0 w-screen max-w-full bg-black h-[5vh] flex'>
-            <div className='left-0 w-1/2 bg-zinc-800 flex justify-start items-center'>
-                <h1 className='text-3xl text-white ml-5'>Name of App</h1>
-            </div>
-            <div className='right-0 w-1/2 bg-zinc-800 flex justify-end items-center'>
-                <HiInformationCircle className='navbar-button'/>
-                <IoLogIn className='navbar-button'/>
-                <AiFillHome className='navbar-button'/>
-            </div>
+        <div className='fixed right-0 bg-zinc-800 flex justify-around items-center rounded-2xl w-[8%] h-[5vh] mt-5 mr-5'>
+            <HiInformationCircle className='navbar-button'/>
+            <IoLogIn className='navbar-button'/>
+            <AiFillHome className='navbar-button'/>
         </div>
     );
 }
