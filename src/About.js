1f7854c2fc5ad9521caca002b8784f02690e357a
Indrajeet2002 <Indrajeet.patwardhan@gmail.com>
2022-12-25 21:31:42 -0800

    update about

--- src/About.js
+++ src/About.js
@@ -3,95 +3,81 @@ import "./About.css";
 
 function About() {
     return (
-      <div className="about">
-        <h1>About Us</h1>
-        <p>This App allows the person to find any movie 
-          they desire to watch. Think of the app Spotify
-          but for movies!  </p>
-        <br />
-        
-        <br />
-        <p>
-        Blah Blah
-        </p>
-        
+      <div className="about flex flex-row items-center">
         <div class="box-content h-32 w-32 p-4 border-4 ...">
-        <h1><strong>Danyal N</strong></h1>
-        <p>
-          Blah Blah
-        </p>
-     
-        
-</div>
-<div class="group flex items-center">
-  <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
-  <div class="ltr:ml-3 rtl:mr-3">
-    <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
-    <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">...</p>
-  </div>
-</div>
-<div class="box-content h-32 w-32 p-4 border-4 ...">
-        <h1> <strong>Joel A</strong></h1>
-        <p>
-          Blah Blah
-        </p>
-     
-        
-</div>
-<div class="group flex items-center">
-  <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
-  <div class="ltr:ml-3 rtl:mr-3">
-    <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
-    <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">.ccc..</p>
-  </div>
-</div>
-<div class="box-content h-32 w-32 p-4 border-4 ...">
-        <h1> <strong>Andy G</strong></h1>
-        <p>
-          Blah Blah
-        </p>
-     
-        
-</div>
-<div class="group flex items-center">
-  <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
-  <div class="ltr:ml-3 rtl:mr-3">
-    <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
-    <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">.ccc..</p>
-  </div>
-</div>
-<div class="box-content h-32 w-32 p-4 border-4 ...">
-        <h1> <strong>Firas A</strong></h1>
-        <p>
-          Blah Blah
-        </p>
-     
-        
-</div>
-<div class="group flex items-center">
-  <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
-  <div class="ltr:ml-3 rtl:mr-3">
-    <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
-    <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">.ccc..</p>
-  </div>
-</div>
-<div class="box-content h-32 w-32 p-4 border-4 ...">
-        <h1> <strong>Indrajeet P</strong></h1>
-        <p>
-          Blah Blah
-        </p>
-     
-        
-</div>
-<div class="group flex items-center">
-  <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
-  <div class="ltr:ml-3 rtl:mr-3">
-    <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
-    <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">.ccc..</p>
-  </div>
-</div>
+          <h1><strong>Danyal N</strong></h1>
+          <p>
+            Blah Blah
+          </p>  
+        </div>
+        <div class="group flex items-center">
+          <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
+          <div class="ltr:ml-3 rtl:mr-3">
+            <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
+            <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">...</p>
+          </div>
+        </div>
+        <div class="box-content h-32 w-32 p-4 border-4 ...">
+                <h1> <strong>Joel A</strong></h1>
+                <p>
+                  Blah Blah
+                </p>
+            
+                
+        </div>
+        <div class="group flex items-center">
+          <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
+          <div class="ltr:ml-3 rtl:mr-3">
+            <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
+            <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">.ccc..</p>
+          </div>
+        </div>
+        <div class="box-content h-32 w-32 p-4 border-4 ...">
+                <h1> <strong>Andy G</strong></h1>
+                <p>
+                  Blah Blah
+                </p>
+            
+                
+        </div>
+        <div class="group flex items-center">
+          <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
+          <div class="ltr:ml-3 rtl:mr-3">
+            <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
+            <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">.ccc..</p>
+          </div>
+        </div>
+        <div class="box-content h-32 w-32 p-4 border-4 ...">
+                <h1> <strong>Firas A</strong></h1>
+                <p>
+                  Blah Blah
+                </p>
+            
+                
+        </div>
+        <div class="group flex items-center">
+          <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
+          <div class="ltr:ml-3 rtl:mr-3">
+            <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
+            <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">.ccc..</p>
+          </div>
+        </div>
+        <div class="box-content h-32 w-32 p-4 border-4 ...">
+                <h1> <strong>Indrajeet P</strong></h1>
+                <p>
+                  Blah Blah
+                </p>
+            
+                
+        </div>
+        <div class="group flex items-center">
+          <img class="shrink-0 h-12 w-12 rounded-full" src="..." alt="" />
+          <div class="ltr:ml-3 rtl:mr-3">
+            <p class="text-sm font-medium text-slate-300 group-hover:text-white">...</p>
+            <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">.ccc..</p>
+          </div>
+        </div>
 
- 
       </div>
       
     );
