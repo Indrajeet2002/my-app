af4273289941145ae9b1c1372d7cfaedabc40999
falshami2002 <alshamifiras@yahoo.com>
2022-12-26 05:36:07 -0800

    Made active navbar icon blue

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,19 +2,27 @@ import './index.css';
 import {AiFillHome} from 'react-icons/ai';
 import {IoLogIn} from 'react-icons/io5';
 import {HiInformationCircle} from 'react-icons/hi2';
-import { Link } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
+import { useEffect, useState } from 'react'; 
 
 const Navbar = () => {
+    const [active, setActive] = useState(["","",""]);
+    const path = useLocation().pathname;
+    useEffect(() => {
+        if(path==="/")
+            setActive(["","","active-button"]);
+    },[])
+
     return (
         <nav className='fixed right-0 bg-zinc-800 flex justify-around items-center rounded-2xl w-[8%] h-[5vh] mt-5 mr-5'>
             <Link to="/">
-                <HiInformationCircle className='navbar-button'/>
+                <HiInformationCircle className={'navbar-button ' + active.at(0)}/>
             </Link>
             <Link to="/">
-                <IoLogIn className='navbar-button'/>
+                <IoLogIn className={'navbar-button ' + active.at(1)}/>
             </Link>
             <Link to="/">
-                <AiFillHome className='navbar-button'/>
+                <AiFillHome className={'navbar-button ' + active.at(2)}/>
             </Link>
         </nav>
     );
