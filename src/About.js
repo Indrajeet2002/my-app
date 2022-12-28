f317d8c0a7c3ca73d3ccbc7972e58d1027070132
Indrajeet2002 <Indrajeet.patwardhan@gmail.com>
2022-12-27 20:46:26 -0800

    spelling change

--- src/About.js
+++ src/About.js
@@ -81,7 +81,7 @@ function About() {
           <div class="text-center text-gray-500 dark:text-gray-400">
               <img class="mx-auto mb-4 w-36 h-36 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="Joseph Avatar"/>
               <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  <a href="#">Indrajeet Pathwarden</a>
+                  <a href="#">Indrajeet Patwardhan</a>
               </h3>
               <p>Worked on Log-In page</p>
               <ul class="flex justify-center mt-4 space-x-4">
