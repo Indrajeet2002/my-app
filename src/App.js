55d7e265207954634d90b04a16a415e1b03625c9
janiljohn <56321852+janiljohn@users.noreply.github.com>
2023-01-01 19:09:11 -0800

    Created landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Footer from "./Footer"
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Signup from "./Signup";
 import Login from "./Login";
 import About from "./About";
+import Landing from "./Landing";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -15,10 +16,10 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Page/>}/>
-      <Route path="/Login" element={<Login/>}/>
-      <Route path="/About" element={<About/>}/>
-      <Route path="/Register" element={<Signup/>}/>
+      <Route path="/" element={<Page />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/Register" element={<Signup />} />
     </Routes>
   );
 }
@@ -26,28 +27,11 @@ function App() {
 function Page() {
   return (
     <div className="App">
-      <Navbar/>
-      {/* {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Signup onFormSwitch={toggleForm}/>
-      } */}
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Footer/>
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
-
-
 
 export default App;
---
+++ src/Landing.js
@@ -0,0 +1,44 @@
+import "./index.css";
+import {Link} from "react-router-dom";
+
+const Landing = () => {
+
+  return (
+    <section className="relative bg-[url(./LandingPageImg.png)] bg-cover bg-center bg-no-repeat">
+        <div class="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-lime-700 sm:to-white/25"></div>
+
+        <div class="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+          <div class="max-w-xl text-center sm:text-left">
+            <h1 class="text-3xl font-extrabold sm:text-5xl">
+              Bring your cinematic interests
+              <strong class="block font-extrabold text-[#1DB954]">
+                To One Place
+              </strong>
+            </h1>
+
+            <p class="mt-4 max-w-lg sm:text-xl sm:leading-relaxed">
+              Create and curate you movie tastes with the help of Movify!
+            </p>
+
+            <div class="mt-8 flex flex-wrap gap-4 text-center">
+              <a
+                href="/Register"
+                class="block w-full rounded bg-[#1DB954] px-12 py-3 text-sm font-medium text-white shadow hover:bg-green-700 focus:outline-none focus:ring active:bg-[#1DB954] sm:w-auto"
+              >
+                Sign Up
+              </a>
+
+              <a
+                href="/About"
+                class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-[#1DB954] shadow hover:text-zinc-900 focus:outline-none focus:ring active:text-[#1DB954] sm:w-auto"
+              >
+                About Us
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+  );
+};
+
+export default Landing;
