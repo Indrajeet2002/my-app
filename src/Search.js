94d0baf291a61173d1a6b162fadcd26a44ae1a7b
Indrajeet2002 <Indrajeet.patwardhan@gmail.com>
2023-01-10 10:48:04 -0800

    search color change

--- src/Search.js
+++ src/Search.js
@@ -6,19 +6,19 @@ const Search = () => {
         
         
             <input type="text" placeholder="Search for a Movie" className="input input-bordered w-full max-w-xs" />
-            <button className="btn">Search</button>
+            <button className="btn bg-[#1DB954]">Search</button>
         
         
         <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
             Current Playlist
         </div>
         <div>
-            Movie 1 <br></br>
-            Movie 2 <br></br>
-            Movie 3 <br></br>
-            Movie 4 <br></br>
-            Movie 5 <br></br>
-            Movie 6 <br></br>
+            Movie 1 <button className="btn bg-[#1DB954]">+</button><br></br><br></br>
+            Movie 2 <button className="btn bg-[#1DB954]">+</button><br></br><br></br>
+            Movie 3 <button className="btn bg-[#1DB954]">+</button><br></br><br></br>
+            Movie 4 <button className="btn bg-[#1DB954]">+</button><br></br><br></br>
+            Movie 5 <button className="btn bg-[#1DB954]">+</button><br></br><br></br>
+            Movie 6 <button className="btn bg-[#1DB954]">+</button><br></br><br></br>
         </div>
     </div>
     
