66e38c60dc22f064e7d165fd39747dfc86d4364b
falshami2002 <alshamifiras@yahoo.com>
2023-01-09 18:17:50 -0800

    No Env

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,24 @@ import { getAuth, onAuthStateChanged} from "firebase/auth"
 import { useContext, useEffect, useState, createContext } from 'react'
 import { getFirestore, doc, setDoc, updateDoc, arrayUnion, getDoc } from 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAFalXZUMqx_UfHvJhulVbmuZkw3kVod5k",
+  authDomain: "group-app-735da.firebaseapp.com",
+  projectId: "group-app-735da",
+  storageBucket: "group-app-735da.appspot.com",
+  messagingSenderId: "387170687398",
+  appId: "1:387170687398:web:3bc6398311c58426e69767",
+  measurementId: "G-SWBHB17PD8"
 };
 
 export const app = initializeApp(firebaseConfig);
