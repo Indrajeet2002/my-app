8cf04f0fcf6d461dc01570944c9651654b1413f7
Indrajeet2002 <Indrajeet.patwardhan@gmail.com>
2023-01-10 18:46:25 -0800

    small changes

--- src/Search.js
+++ src/Search.js
@@ -1,31 +1,50 @@
 import "./index.css";
 
 const Search = () => {
+    const movies = [];
+    movies.push("Movie 1", "Movie 2", "Movie 3");
+    
+    
   return (
-    <div>
+    <>
         <input type="text" placeholder="Search for a Movie" className="input input-bordered w-full max-w-xs" />
         <button className="btn bg-[#1DB954] text-white">🔍</button>
-        <div>
-            <button className="btn">Movie 1</button> <button className="btn bg-[#1DB954] text-white">+</button><br></br><br></br>
-            <button className="btn">Movie 2</button> <button className="btn bg-[#1DB954] text-white">+</button><br></br><br></br>
-            <button className="btn">Movie 3</button> <button className="btn bg-[#1DB954] text-white">+</button><br></br><br></br>
-            <button className="btn">Movie 4</button> <button className="btn bg-[#1DB954] text-white">+</button><br></br><br></br>
-            <button className="btn">Movie 5</button> <button className="btn bg-[#1DB954] text-white">+</button><br></br><br></br>
-            <button className="btn">Movie 6</button> <button className="btn bg-[#1DB954] text-white">+</button><br></br><br></br>
-        </div>
+        <add className="add">
+            <ul>
+                <li className="py-3">
+                    <button className="btn">Movie 1</button> <button className="btn bg-[#1DB954] text-white">+</button>
+                </li>
+                <li className="pb-3">
+                    <button className="btn">Movie 2</button> <button className="btn bg-[#1DB954] text-white">+</button>
+                </li>
+                <li className="pb-3">
+                    <button className="btn">Movie 3</button> <button className="btn bg-[#1DB954] text-white">+</button>
+                </li>
+                <li className="pb-3">
+                    <button className="btn">Movie 4</button> <button className="btn bg-[#1DB954] text-white">+</button>
+                </li>
+                <li className="pb-3">
+                    <button className="btn">Movie 5</button> <button className="btn bg-[#1DB954] text-white">+</button>
+                </li>
+                <li className="pb-3">
+                    <button className="btn">Movie 6</button> <button className="btn bg-[#1DB954] text-white">+</button>
+                </li>
+
+            </ul>
+        </add>
         
         <div className="grid h-20 flex-grow card bg-base-300 rounded-box place-items-center">
             Current Playlist
         </div>
-        <div>
-            Movie 1 <br></br><br></br>
-            Movie 2 <br></br><br></br>
-            Movie 3 <br></br><br></br>
-            Movie 4 <br></br><br></br>
-            Movie 5 <br></br><br></br>
-            Movie 6 <br></br><br></br>
-        </div>
-    </div>
+        <ul>
+            <li className="py-3">Movie 1</li>
+            <li className="pb-3">Movie 2</li>
+            <li className="pb-3">Movie 3</li>
+            <li className="pb-3">Movie 4</li>
+            <li className="pb-3">Movie 5</li>
+            <li className="pb-3">Movie 6</li>
+        </ul>
+    </>
     
   );
 };
