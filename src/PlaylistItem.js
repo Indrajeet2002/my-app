897257b21b48e12a46d8f0958d392334d6b3c3e0
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2023-01-12 19:26:51 -0800

    red

--- src/PlaylistItem.js
+++ src/PlaylistItem.js
@@ -10,7 +10,7 @@ function PlaylistItem(props) {
     <div classname= 'w-full flex justify-center'>
       <div className="py-3  flex w-full">
         <button className="btn w-[95%]">{props.info}</button>
-        <button className="btn bg-[#1DB954] text-white w-[5%]" onClick={handleClick}>X</button>
+        <button className="btn bg-[#b91d1d] text-white w-[5%]" onClick={handleClick}>X</button>
       </div>
     </div>
   )
